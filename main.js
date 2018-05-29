2eb00a2183727686f426b0e1d3f55a07642e2f7a
杨清 <yangqing1989@icloud.com>
2018-05-29 16:28:11 +0800

    change
    change

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 /**
  * yqing
  */
-defineClass('SGAdverPageViewController', {
+defineClass('ViewController', {
     viewDidLoad: function() {
         self.ORIGviewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
         {
-            console.log("[yqing]<main.js viewDidLoad> success!");
+            console.log("[yqing]<main.js viewDidLoad> success! 20180529");
             var alertView = require('UIAlertView').alloc().init();
             alertView.setTitle('Alert');
             alertView.setMessage('AlertView from js, author yqing');
